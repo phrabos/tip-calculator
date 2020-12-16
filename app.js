d3a73c06381bed9fd32178110ccbb04bf58f08ae
Patrick Hrabos <phrabos@gmail.com>
2020-12-15 22:52:21 -0500

    add error message and red formatting; add $ to tip amount display

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const billAmount = document.getElementById("bill-amount");
 const serviceSelector = document.getElementById("service-selector");
+const nil = document.getElementById("nil");
 const ten = document.getElementById("ten");
 const twelve = document.getElementById("twelve");
 const fifteen = document.getElementById("fifteen");
@@ -17,28 +18,32 @@ button.addEventListener("click", () => {
 
     switch (serviceSelector.value) {
         case ten.value:
-            tipDisplay.innerText = (Number(billAmount.value) * Number(ten.value)).toFixed(2);            
+            tipDisplay.textContent = `$${(Number(billAmount.value) * Number(ten.value)).toFixed(2)}`;            
             break;
         case twelve.value:
-            tipDisplay.innerText = (Number(billAmount.value) * Number(twelve.value)).toFixed(2);    
+            tipDisplay.textContent = `$${(Number(billAmount.value) * Number(twelve.value)).toFixed(2)}`;    
             break;
             case fifteen.value:
-            tipDisplay.innerText = (Number(billAmount.value) * Number(fifteen.value)).toFixed(2);    
+            tipDisplay.textContent = `$${(Number(billAmount.value) * Number(fifteen.value)).toFixed(2)}`;    
             break;
             case eighteen.value:
-            tipDisplay.innerText = (Number(billAmount.value) * Number(eighteen.value)).toFixed(2);    
+            tipDisplay.textContent = `$${(Number(billAmount.value) * Number(eighteen.value)).toFixed(2)}`;    
             break;
             case twenty.value:
-            tipDisplay.innerText = (Number(billAmount.value) * Number(twenty.value)).toFixed(2);    
+            tipDisplay.textContent = `$${(Number(billAmount.value) * Number(twenty.value)).toFixed(2)}`;    
             break;
             case twentyFive.value:
-            tipDisplay.innerText = (Number(billAmount.value) * Number(twentyFive.value)).toFixed(2);    
+            tipDisplay.textContent = `$${(Number(billAmount.value) * Number(twentyFive.value)).toFixed(2)}`;    
             break;
-        default: tipDisplay.innerText = "$1000"
+        default: tipDisplay.textContent = "error: select quality of service"
             break;
     }
     
-    
+    if (serviceSelector.value == ""){
+        tipDisplay.classList="red";
+    } else {
+        tipDisplay.classList="tip-display";
+    }
 
 
 });
